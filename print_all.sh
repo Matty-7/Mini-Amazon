2be4326ba1e04b528d6785025b2c9aa6129bc345
Matty-7 <jh730@duke.edu>
2025-04-23 19:47:34 -0400

    print_all

---
+++ print_all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+output_file="output.txt"
+
+> "$output_file"
+
+while IFS= read -r -d '' path; do
+  if [ -d "$path" ]; then
+    echo "${path}/" >> "$output_file"
+  else
+    if [[ "$path" == *.py ]]; then
+      echo -e "\n--- File: $path ---" >> "$output_file"
+      cat "$path" >> "$output_file"
+    fi
+  fi
+done < <(find . -print0)
+
+echo "Saved to $output_file"
