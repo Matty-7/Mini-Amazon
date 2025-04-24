93553b8e51f543017ea3fbd48f8dd2bf47b04269
Matty-7 <jh730@duke.edu>
2025-04-23 22:35:41 -0400

    update .sh

--- print_all.sh
+++ print_all.sh
@@ -13,7 +13,7 @@ while IFS= read -r -d '' path; do
   if [ -d "$path" ]; then
     echo "${path}/" >> "$output_file"
   else
-    if [[ "$path" == *.py || "$path" == *.proto ]]; then
+    if [[ "$path" == *.py || "$path" == *.proto || "$path" == *.sh ]]; then
       echo -e "\n--- File: $path ---" >> "$output_file"
       cat "$path" >> "$output_file"
     fi
